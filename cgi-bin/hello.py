5a087ef733ad9380a0537125c6844667cbfc3157
editorbank <>
2021-12-22 00:30:49 +0300

    v1.0

--- cgi-bin/hello.py
+++ cgi-bin/hello.py
@@ -1,3 +1,3 @@
 #!/usr/local/bin/python
 print("Content-Type: text/html\n")
-print("<!doctype html><title>Hello</title><h2>hello world</h2>")
+print("<!doctype html><title>Hello</title><h2>Hello, world from Python!</h2>")
